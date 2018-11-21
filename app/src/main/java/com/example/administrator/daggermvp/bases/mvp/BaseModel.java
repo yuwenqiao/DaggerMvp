af97dac292df668b8afa63897795f9f8f0d13968
yudonghui <842958599@qq.com>
2018-11-21 16:30:22 +0800

    提交BaseModel类

--- app/src/main/java/com/example/administrator/daggermvp/bases/mvp/BaseModel.java
+++ app/src/main/java/com/example/administrator/daggermvp/bases/mvp/BaseModel.java
@@ -5,16 +5,13 @@ import android.util.Log;
 import com.example.administrator.daggermvp.utils.RetrofitHelper;
 
 public class BaseModel implements IModel {
-    private RetrofitHelper retrofitHelper;
+    protected RetrofitHelper retrofitHelper;
     public BaseModel(RetrofitHelper retrofitHelper){
         this.retrofitHelper=retrofitHelper;
-        Log.i("dagger2","retrofitHelper------"+retrofitHelper);
     }
 
     @Override
     public void onDestroy() {
-        Log.i("dagger2","ondestroy11------"+retrofitHelper);
         retrofitHelper=null;
-        Log.i("dagger2","ondestroy22------"+retrofitHelper);
     }
 }
